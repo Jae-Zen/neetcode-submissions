9ae1d68ddf90706d4746c15d97884d858c16a3f0
Jae-Zen <118703545+Jae-Zen@users.noreply.github.com>
2026-09-06 00:58:35 +0800

    Add: top-k-elements-in-list - submission-10

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-10.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freq = {};
+        for (let num of nums) {
+            freq[num] = (freq[num] ?? 0) + 1;
+        }
+        const heap = new MinPriorityQueue((x) => x[1])
+        for (let item of Object.entries(freq)) {
+            heap.enqueue(item);
+            if (heap.size() > k) heap.dequeue();
+        }
+        return heap.toArray().map(item => Number(item[0]))
+    }
+}
